f56cdf462736f5499f349cba2c2d9a267dd7df04
RupakVeerla <54211182+RupakVeerla@users.noreply.github.com>
2021-06-04 18:50:02 +0530

    Solution

--- 11-if/exercises/09-days-in-month/main.go
+++ 11-if/exercises/09-days-in-month/main.go
@@ -8,6 +8,13 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Days in a Month
 //
@@ -88,4 +95,28 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Give me a month name")
+		return
+	}
+
+	year := time.Now().Year()
+	leap := year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	month := strings.ToLower(os.Args[1])
+	days := 28
+
+	if month == "april" || month == "june" || month == "september" || month == "november" {
+		days = 30
+	} else if month == "january" || month == "march" || month == "may" || month == "july" || month == "august" || month == "october" || month == "december" {
+		days = 31
+	} else if month == "february" {
+		if leap {
+			days = 29
+		}
+	} else {
+		fmt.Printf("%q is not a month.\n", month)
+		return
+	}
+
+	fmt.Printf("%q has %d days.\n", month, days)
 }
