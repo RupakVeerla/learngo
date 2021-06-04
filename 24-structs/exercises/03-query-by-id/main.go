c94c238f87455b7e05afe8a4917d28da7923eab3
RupakVeerla <54211182+RupakVeerla@users.noreply.github.com>
2021-06-04 14:52:46 +0530

    Solution

--- 24-structs/exercises/03-query-by-id/main.go
+++ 24-structs/exercises/03-query-by-id/main.go
@@ -43,6 +43,69 @@ package main
 //  list, quit, and id commands to see it in action.
 // ---------------------------------------------------------
 
-func main() {
-	// use your solution from the previous exercise
+type item struct {
+	id, price int
+	name      string
 }
+
+type game struct {
+	item
+	genre string
+}
+
+func main() {
+	games := []game{
+		{item: item{id: 1, name: "god of war", price: 50}, genre: "action adventure"},
+		{item: item{id: 2, name: "x-com 2", price: 30}, genre: "strategy"},
+		{item: item{id: 3, name: "minecraft", price: 20}, genre: "sandbox"},
+	}
+
+	in := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf(`
+> list - list all the games
+> Id id - Game id 
+> quit - to exit
+
+`)
+		in.Scan()
+
+		fmt.Println()
+
+		input := strings.Fields(in.Text())
+	loop:
+		switch input[0] {
+		case "list":
+			fmt.Printf("#ID   %-15s %-18s Price\n", "Game", "Genre")
+
+			for _, game := range games {
+				fmt.Printf("#%d: %-15q %-20s $%d\n", game.id, game.name, "("+game.genre+")", game.price)
+			}
+		case "quit":
+			fmt.Println("Exiting!")
+			return
+		case "id":
+			if len(input) != 2 {
+				fmt.Println("Wrong input")
+				break
+			}
+
+			id, err := strconv.Atoi(input[1])
+			if err != nil {
+				fmt.Println("Wrong input: ", err)
+				break
+			}
+
+			for _, game := range games {
+				if id == game.id {
+					fmt.Printf("  %-15s %-18s Price\n", "Game", "Genre")
+					fmt.Printf("%-15q %-20s $%d\n", game.name, "("+game.genre+")", game.price)
+					break loop
+				}
+			}
+			fmt.Println("No game found for ID: ", id)
+		}
+	}
+}
+
